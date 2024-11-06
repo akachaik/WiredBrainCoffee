6ddfd4227f817158ff4f1bb4dc0fd38aff894f92
akachai <akachai@mail.com>
2024-11-07 00:27:07 +0700

    Use IDbContextFactory

--- WiredBrainCoffee.EmployeeManager/Program.cs
+++ WiredBrainCoffee.EmployeeManager/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddDbContext<EmployeeManagerDbContext>(options =>
+builder.Services.AddDbContextFactory<EmployeeManagerDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
